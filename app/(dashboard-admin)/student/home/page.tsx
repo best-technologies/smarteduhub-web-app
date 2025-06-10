4eb191cce03471183ceaf81972065c71b6201337
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-06-10 12:31:38 +0100

    Feature: add the layout structure for the student dashboard.

---
+++ app/(dashboard-admin)/student/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StudentsHomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Students Dashboard Home</h1>
+      {/* Add your dashboard content here */}
+    </div>
+  );
+};
+
+export default StudentsHomePage;
