0f54eb1e307b9cdf078db7a6609afb28ef05172c
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-06-10 12:37:25 +0100

    Feature: setup the structure for the teacher dashboard.

---
+++ app/(dashboard-admin)/teacher/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TeacherDashboardPage: React.FC = () => {
+  return (
+    <main>
+      <h1>Teacher Dashboard</h1>
+      {/* Add dashboard components here */}
+    </main>
+  );
+};
+
+export default TeacherDashboardPage;
